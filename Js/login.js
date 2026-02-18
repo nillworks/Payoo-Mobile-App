fc4446ab3ae4708351dd015886fa7b7f7b4bd6e3
Shipon Roy <shiponworks@gmail.com>
2026-02-18 06:17:26 +0600

    Completes: login page ui and login function add

---
+++ Js/login.js
@@ -0,0 +1,15 @@
+const loginButton = document.getElementById('login-btn');
+
+loginButton.addEventListener('click', () => {
+  // mobile number get And pin Number get
+  const numberInput = document.getElementById('number_input');
+  const pinInput = document.getElementById('pin_input');
+
+  // match pin and mobile number user login
+  if (numberInput.value === '01612475642' && Number(pinInput.value) === 1234) {
+    console.log('Login Success');
+    window.location.assign('../Html/home.html');
+  } else {
+    console.log('Login Failed');
+  }
+});
